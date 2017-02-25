62c5e0bdbd7d983ca3bec202df604f183b3dc55c
Buddy Magsipoc <keikun17@gmail.com>
2017-02-25 11:29:22 +0800

    Init。

---
+++ index.vr.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  StyleSheet,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+class WelcomeToVR extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('chess-world.jpg')}/>
+        <Text
+          style={{
+            backgroundColor:'blue',
+            padding: 0.02,
+            textAlign:'center',
+            textAlignVertical:'center',
+            fontSize: 0.8,
+            layoutOrigin: [0.5, 0.5],
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+          Jet Fuel can't melt steel beams 
+          Jet Fuel can't melt steel beams
+          Jet Fuel can't melt steel beams
+          Jet Fuel can't melt steel beams
+          Jet Fuel can't melt steel beams
+          Jet Fuel can't melt steel beams
+        </Text>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
